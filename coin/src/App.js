4ff1e82d44fd17d34136a2f3e58ce9cf9bfccdb9
whara123 <64121533+whara123@users.noreply.github.com>
2022-01-23 20:22:52 +0900

    :sparkles: Add studt file(coin)

---
+++ coin/src/App.js
@@ -0,0 +1,28 @@
+import { useState,useEffect } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([])
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) =>response.json())
+    .then((json)=>{
+      setCoins(json);
+      setLoading(false);
+    });
+  },[])
+  return (
+    <div>
+      <h1>The coins!</h1>
+      {loading ? <strong>Loading...</strong>:null}
+      <ul>
+        {coins.map((coin)=>
+          <li key={coin.id}>
+            {coin.name}({coin.symbol}) : ${coin.quotes.USD.price}
+          </li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
